d5581a69fc271a42df4b037c57bdf5e6ffbe6d4a
hossainshadat <hossainshadat501@gmail.com>
2022-10-27 19:05:47 +0600

    Server seide data almost done

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("Courses API Running");
+});
+
+app.get("/courses-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "07") {
+    res.send(courses);
+  } else {
+    const category_courses = courses.filter((n) => n.category_id === id);
+    res.send(category_courses);
+  }
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = news.find((n) => n._id === id);
+  res.send(selectedCourses);
+});
+
+app.listen(port, () => {
+  console.log("Mern Development Server is running on Port:", port);
+});
